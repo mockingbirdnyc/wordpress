66f664ed6527f98d34bc746507e4ca03cc6695e2
mockingbirdnyc <info@mbird.com>
2017-12-21 08:09:11 -0500

    tweaks

--- wp-config.php
+++ wp-config.php
@@ -93,6 +93,7 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define( 'WP_MEMORY_LIMIT', '256M' );
 define( 'WP_MAX_MEMORY_LIMIT', '256M' );
 
 /**#@+
