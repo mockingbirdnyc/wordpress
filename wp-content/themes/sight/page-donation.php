c34c330481ff757a06ee44bf6115f75b070a6df6
Brian McMillen <brian.mcmillen@explorelearning.com>
2018-09-30 11:30:18 -0400

    update the donation page to have a $10 minimum monthly donation

--- wp-content/themes/sight/page-donation.php
+++ wp-content/themes/sight/page-donation.php
@@ -11,57 +11,56 @@
 
         <div class="entry">
             <div <?php post_class('single clear'); ?> id="post_<?php the_ID(); ?>">
-              
-				 
-				
+
+
+
                 <div class="post-content">
 				<?php the_content(); ?>
 				<h2>You may give a one-time donation or setup a recurring monthly donation. We use the PayPal.com giving tools, so <enm>your donations are always safe and secure.</em></h2>
 <p class="text"><strong>Option 1: One-time Donation</strong></p>
 
-<form class="input2a clearfix" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_donations" /> 
-<input type="hidden" name="business" value="mockingbirdnyc@gmail.com" /> 
-<input type="hidden" name="item_name" value="One-Time Donation" /> 
-<input type="hidden" name="item_number" value="2018" /> 
-<div><span class="inputtext">$</span><input class="input3" type="text" name="amount" value="" size="10"/> <span class="text">Please enter an amount to be charged/debited from your account one time.</span></div> 
+<form class="input2a clearfix" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+<input type="hidden" name="cmd" value="_donations" />
+<input type="hidden" name="business" value="mockingbirdnyc@gmail.com" />
+<input type="hidden" name="item_name" value="One-Time Donation" />
+<input type="hidden" name="item_number" value="2018" />
+<div><span class="inputtext">$</span><input class="input3" type="text" name="amount" value="" size="10"/> <span class="text">Please enter an amount to be charged/debited from your account one time.</span></div>
 <br />
-<input type="hidden" name="no_shipping" value="2" /> 
-<input type="hidden" name="no_note" value="1" /> 
-<input type="hidden" name="currency_code" value="USD" /> 
-<input type="hidden" name="tax" value="0" /> 
-<input type="hidden" name="bn" value="Donate" /> 
+<input type="hidden" name="no_shipping" value="2" />
+<input type="hidden" name="no_note" value="1" />
+<input type="hidden" name="currency_code" value="USD" />
+<input type="hidden" name="tax" value="0" />
+<input type="hidden" name="bn" value="Donate" />
 <input class="input2a" type="image" name="submit" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" alt="PayPal - The safer, easier way to pay online!" />
 </form>
 
 <hr noshade="noshade" size="1" class="clearfix" />
 <p class="text"><strong>Option 2: Setup Recurring Monthly Donation.</strong> All monthly donors to Mockingbird receive a complimentary subscription to our quarterly magazine, The Mockingbird.</p>
 
-<form class="input2 clearfix" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_xclick-subscriptions" /> 
-<input type="hidden" name="business" value="mockingbirdnyc@gmail.com" /> 
-<input type="hidden" name="item_name" value="Recurring Donation" /> 
-<input type="hidden" name="no_shipping" value="1" /> 
-<input type="hidden" name="no_note" value="1" /> 
-<input type="hidden" name="modify" value="1" /> 
-<input type="hidden" name="currency_code" value="USD" /> 
-<input type="hidden" name="lc" value="US" /> 
-<input type="hidden" name="bn" value="Subscribe" /> 
-<div><span class="inputtext">$</span><input class="input3" type="text" name="a3" id="a3" value="" size="10" /> <span class="text">Please enter recurring amount to be charged/debited from your credit or debit card each month <span id="flash">(Minimum of $5)</span>.</span> </div>
+<form class="input2 clearfix" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+<input type="hidden" name="cmd" value="_xclick-subscriptions" />
+<input type="hidden" name="business" value="mockingbirdnyc@gmail.com" />
+<input type="hidden" name="item_name" value="Recurring Donation" />
+<input type="hidden" name="no_shipping" value="1" />
+<input type="hidden" name="no_note" value="1" />
+<input type="hidden" name="modify" value="1" />
+<input type="hidden" name="currency_code" value="USD" />
+<input type="hidden" name="lc" value="US" />
+<input type="hidden" name="bn" value="Subscribe" />
+<div><span class="inputtext">$</span><input class="input3" type="text" name="a3" id="a3" value="" size="10" /> <span class="text">Please enter recurring amount to be charged/debited from your credit or debit card each month <span id="flash">(Minimum of $10)</span>.</span> </div>
 <br />
-<input type="hidden" name="p3" value="1" /> 
-<input type="hidden" name="t3" value="M" /> 
+<input type="hidden" name="p3" value="1" />
+<input type="hidden" name="t3" value="M" />
 <input type="hidden" name="src" value="1" />
-<input type="hidden" name="sra" value="1" /> 
+<input type="hidden" name="sra" value="1" />
 <input class="input2" type="image" name="submit" src="https://www.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif" alt="PayPal - The safer, easier way to pay online!" />
 <img src="https://www.paypal.com/en_US/i/scr/pixel.gif" alt="" width="1" height="1" border="0" />
 </form>
 <script>
-function validate_form ( ) {
-	
-	valid = true;
-	if ( jQuery("#a3").val() < 5 ){
-		alert ( "Donation minimum is $5 a month" );
+function validate_form () {
+	var valid = true;
+	if ( jQuery("#a3").val() < 10 ){
+		alert ( "Donation minimum is $10 a month" );
 		valid = false;
 	}
 	return valid;
@@ -86,7 +85,7 @@ jQuery(".input2").submit(validate_form);
 <p class="text"><strong>Q7. Can Mockingbord change my recurring or single donation amounts for me?</strong></p>
 <p class="text">No. Please make all changes through the PayPal interface. Mockingbird is unable to change individual PayPal accounts. To contact PayPal please visit their <a class="text" href="https://www.paypal.com/us/cgi-bin/helpscr?cmd=_help&amp;t=escalateTab" target="_blank">Contact Us</a> website.</p>
 				</div>
-            
+
 			</div>
         </div>
 
