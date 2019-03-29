04877d5e316a836146db99219e494968217917b9
Brian McMillen <brian.mcmillen@explorelearning.com>
2019-03-28 20:39:59 -0400

    use the revelvanssi_the_title function to highlight search result titles

--- wp-content/themes/sight/loop.php
+++ wp-content/themes/sight/loop.php
@@ -1,7 +1,7 @@
 ﻿<?php if ( have_posts() ) : ?>
     <div id="loop" class="list clear">
 
-    <?php while ( have_posts() ) : the_post(); 
+    <?php while ( have_posts() ) : the_post();
 		global $excerpt_checkbox_mb;
 		$exmeta = $excerpt_checkbox_mb->the_meta();
 		if ( !is_array($exmeta) ){
@@ -20,7 +20,7 @@
 
         <div <?php post_class('post clear'); ?> id="post_<?php the_ID(); ?>">
 
-		<?php 
+		<?php
 
 
 		if ( $exmeta['cb_single'] != "yes" && $word_count ){
@@ -29,17 +29,27 @@
 				'image_class' => 'thumb',
 				'callback' => 'find_image'
 			));
-			} 
+			}
 			?>
-           
-            <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+            <h2>
+				<a href="<?php the_permalink() ?>">
+					<?php
+						if (function_exists('relevanssi_the_title')) {
+							relevanssi_the_title();
+						} else {
+							the_title();
+						}
+					?>
+				</a>
+			</h2>
 
             <div class="post-meta">by <span class="post-author"><a
                     href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" title="Posts by <?php the_author(); ?>"><?php the_author(); ?></a></span>
                                    on <span
                         class="post-date"><?php the_time(__('M j, Y')) ?></span> <em>&bull; </em><?php comments_popup_link(__(''), __('1 Comment'), __('% Comments'), '', __('Comments Closed')); ?> <?php edit_post_link( __( 'Edit entry'), '<em>&bull; </em>'); ?>
             </div>
-            <?php 
+            <?php
 			$image = find_image();
 			?>
             <!--if full post, add left padding to avoid image wrap-->
@@ -47,14 +57,14 @@
             <div class="post-content">
 			<?php the_content();
 			elseif ($image) : ?>
-            <div class="post-content post-psuedo-col"> 
+            <div class="post-content post-psuedo-col">
             <?php the_excerpt();
 				else : ?>
-             <div class="post-content"> 
+             <div class="post-content">
 			<?php the_excerpt();
 				endif; ?>
             </div>
-					
+
         </div>
 
     <?php endwhile; ?>
